c0ebf7d612aa47ffd54247236bbc53a197c46a77
Grayson Keaton <gkeaton23@gmail.com>
2024-08-02 01:02:26 +0200

    completed tasks 1-3 with sucessful tests

--- message.js
+++ message.js
@@ -1,5 +1,11 @@
 class Message {
-   // Write code here!
+   constructor(name, commands) {
+      if (!name) {
+         throw('Error: Please pass in a constructor!');
+      }
+      this.name = name;
+      this.commands = commands;
+   }
 }
 
 module.exports = Message;
--- spec/message.spec.js
+++ spec/message.spec.js
@@ -5,5 +5,21 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Message class", function() {
+    it('throws error if a name is NOT passed into the constructor as the first parameter', function() {
+        expect(function () {
+            new Message();
+        }).toThrow("Error: Please pass in a constructor!");
+    });
+
+    it('constructor sets name', function() {
+        let test = new Message('testName');
+        expect(test.name).toBe('testName');
+    });
+
+    it('contains a commands array passed into the constructor as the 2nd argument', function() {
+        let arr = ['message 1', 'message 2', 'message 3']
+        let test = new Message('testName', arr);
+        expect(test.commands).toStrictEqual([ 'message 1', 'message 2', 'message 3' ]);
+    });
 
 });
